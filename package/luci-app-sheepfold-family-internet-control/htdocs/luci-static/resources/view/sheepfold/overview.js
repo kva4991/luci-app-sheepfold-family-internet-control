649db41167673895d658663a30eedd040331c037
kva4991 <kva4991@users.noreply.github.com>
2026-07-05 12:18:16 +0300

    Avoid initial WAN restored log

--- package/luci-app-sheepfold-family-internet-control/htdocs/luci-static/resources/view/sheepfold/overview.js
+++ package/luci-app-sheepfold-family-internet-control/htdocs/luci-static/resources/view/sheepfold/overview.js
@@ -4907,7 +4907,7 @@ return view.extend({
         },
 
         render: function () {
-                var assetVersion = '0.1.0-74';
+                var assetVersion = '0.1.0-75';
                 var self = this;
                 var internetBlocked = this.isGlobalInternetBlocked();
                 var allowlistCount = devices.filter(function (device) { return device.status === 'allow'; }).length;
