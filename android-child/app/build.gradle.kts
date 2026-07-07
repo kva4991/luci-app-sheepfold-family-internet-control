8825cb3d4288f2e6527ca3a4c63f66a947ed6c68
kva4991 <31066698+kva4991@users.noreply.github.com>
2026-07-08 00:37:28 +0300

    android-child: add explicit ViewModel coroutine support

--- android-child/app/build.gradle.kts
+++ android-child/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
