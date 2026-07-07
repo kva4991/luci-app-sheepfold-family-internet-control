9560dbfb35a78f8359722835fabc45a6b992e69c
kva4991 <31066698+kva4991@users.noreply.github.com>
2026-07-08 00:03:40 +0300

    android-child: remove missing version catalog dependency

--- android-child/build.gradle.kts
+++ android-child/build.gradle.kts
@@ -1,6 +1,6 @@
-// Top-level build file for SheepfoldChild app
+// Top-level build file for SheepfoldChild app.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
